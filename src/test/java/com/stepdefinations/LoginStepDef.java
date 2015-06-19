1fd95665a911fbd1c4c319da758019558b51a2de
biswajit Samal <bswjitsamal@gmail.com>
2015-06-19 18:35:18 +0530

    My 3rd commit

--- src/test/java/com/stepdefinations/LoginStepDef.java
+++ src/test/java/com/stepdefinations/LoginStepDef.java
@@ -1,5 +1,43 @@
 package com.stepdefinations;
 
+import cucumber.api.java.en.*;
+
 public class LoginStepDef {
+
+	@Given("^I am in the home page$")
+	public void i_am_in_the_home_page() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("^I click on login link$")
+	public void i_click_on_login_link() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^I should in login page$")
+	public void i_should_in_login_page() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Given("^I am in the login page$")
+	public void i_am_in_the_login_page() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@When("^I enter valid credentials$")
+	public void i_enter_valid_credentials() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
+
+	@Then("^I see the dashboard page$")
+	public void i_see_the_dashboard_page() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+
+	}
 
 }
--- src/test/java/com/stepdefinations/SignUpStepDef.java
+++ src/test/java/com/stepdefinations/SignUpStepDef.java
@@ -1,5 +1,33 @@
 package com.stepdefinations;
 
+import cucumber.api.java.en.*;
+
 public class SignUpStepDef {
+	
+
+@Given("^I am in the ThinkHr web page$")
+public void i_am_in_the_ThinkHr_web_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@When("^I click on SignUp button$")
+public void i_click_on_SignUp_button() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@When("^Enter valid credentials then hit submit$")
+public void enter_valid_credentials_then_hit_submit() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@Then("^I see the success page$")
+public void i_see_the_success_page() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
 
 }
