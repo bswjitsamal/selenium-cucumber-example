297292ef1c2563df309b93f5e6ebaf317b79b994
biswajit Samal <bswjitsamal@gmail.com>
2015-06-22 18:37:19 +0530

    My 2nd day end commit

--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -1,4 +1,426 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("signup.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("contactus.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Testing for contactus page",
+  "description": "",
+  "id": "testing-for-contactus-page",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 4,
+  "name": "contactus page",
+  "description": "",
+  "id": "testing-for-contactus-page;contactus-page",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@contactus"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I am in the contactus page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter \"\u003csubjectheading\u003e\" form dropdown",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter \"\u003cemail\u003e\" in email field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter \"\u003corderreferance\u003e\" in order field",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter \"\u003cmessage\u003e\" in text area",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click on send",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I should a success message",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 13,
+  "name": "",
+  "description": "",
+  "id": "testing-for-contactus-page;contactus-page;",
+  "rows": [
+    {
+      "cells": [
+        "subjectheading",
+        "email",
+        "orderreferance",
+        "message"
+      ],
+      "line": 14,
+      "id": "testing-for-contactus-page;contactus-page;;1"
+    },
+    {
+      "cells": [
+        "Customer service",
+        "t@t.com",
+        "12345",
+        "Hello World"
+      ],
+      "line": 15,
+      "id": "testing-for-contactus-page;contactus-page;;2"
+    },
+    {
+      "cells": [
+        "Webmaster",
+        "t@t.com",
+        "67890",
+        "Hello Ruby"
+      ],
+      "line": 16,
+      "id": "testing-for-contactus-page;contactus-page;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "contactus page",
+  "description": "",
+  "id": "testing-for-contactus-page;contactus-page;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@contactus"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I am in the contactus page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter \"Customer service\" form dropdown",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter \"t@t.com\" in email field",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter \"12345\" in order field",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter \"Hello World\" in text area",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click on send",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I should a success message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_am_in_the_contactus_page()"
+});
+formatter.result({
+  "duration": 11818707910,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Customer service",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_form_dropdown(String)"
+});
+formatter.result({
+  "duration": 2491561855,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "t@t.com",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_email_field(String)"
+});
+formatter.result({
+  "duration": 131505476,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "12345",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_order_field(String)"
+});
+formatter.result({
+  "duration": 85978047,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Hello World",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_text_area(String)"
+});
+formatter.result({
+  "duration": 596289207,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_click_on_send()"
+});
+formatter.result({
+  "duration": 1315426970,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_should_a_success_message()"
+});
+formatter.result({
+  "duration": 39342,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "contactus page",
+  "description": "",
+  "id": "testing-for-contactus-page;contactus-page;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@contactus"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I am in the contactus page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter \"Webmaster\" form dropdown",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter \"t@t.com\" in email field",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "I enter \"67890\" in order field",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I enter \"Hello Ruby\" in text area",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I click on send",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I should a success message",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_am_in_the_contactus_page()"
+});
+formatter.result({
+  "duration": 2287453196,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Webmaster",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_form_dropdown(String)"
+});
+formatter.result({
+  "duration": 1860955597,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "t@t.com",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_email_field(String)"
+});
+formatter.result({
+  "duration": 113375378,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "67890",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_order_field(String)"
+});
+formatter.result({
+  "duration": 92455770,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Hello Ruby",
+      "offset": 9
+    }
+  ],
+  "location": "ContactUsStepDef.i_enter_in_text_area(String)"
+});
+formatter.result({
+  "duration": 135497136,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_click_on_send()"
+});
+formatter.result({
+  "duration": 1862944243,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsStepDef.i_should_a_success_message()"
+});
+formatter.result({
+  "duration": 33184,
+  "status": "passed"
+});
+formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Testing for login page",
+  "description": "",
+  "id": "testing-for-login-page",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Check successfull login page",
+  "description": "",
+  "id": "testing-for-login-page;check-successfull-login-page",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@login"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "I am in the login page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter valid credentials",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "I see the dashboard page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDef.i_am_in_the_login_page()"
+});
+formatter.result({
+  "duration": 1858753821,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDef.i_enter_valid_credentials()"
+});
+formatter.result({
+  "duration": 4980527489,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDef.i_see_the_dashboard_page()"
+});
+formatter.result({
+  "duration": 43105,
+  "status": "passed"
+});
+formatter.uri("signup.feature");
 formatter.feature({
   "line": 1,
   "name": "As a user I want to Testing SignUp page",
@@ -44,26 +466,28 @@ formatter.match({
   "location": "SignUpStepDef.i_am_in_the_automationpractice_web_page()"
 });
 formatter.result({
-  "duration": 10692961399,
-  "error_message": "org.openqa.selenium.remote.UnreachableBrowserException: Error communicating with the remote browser. It may have died.\nBuild info: version: \u00272.46.0\u0027, revision: \u002761506a4624b13675f24581e453592342b7485d71\u0027, time: \u00272015-06-04 10:22:50\u0027\nSystem info: host: \u0027Biswajit-PC\u0027, ip: \u0027192.168.10.148\u0027, os.name: \u0027Windows 7\u0027, os.arch: \u0027amd64\u0027, os.version: \u00276.1\u0027, java.version: \u00271.8.0_25\u0027\nDriver info: driver.version: EventFiringWebDriver\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:599)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:306)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:483)\r\n\tat org.openqa.selenium.support.events.EventFiringWebDriver$2.invoke(EventFiringWebDriver.java:102)\r\n\tat com.sun.proxy.$Proxy15.get(Unknown Source)\r\n\tat org.openqa.selenium.support.events.EventFiringWebDriver.get(EventFiringWebDriver.java:160)\r\n\tat com.stepdefinations.SignUpStepDef.i_am_in_the_automationpractice_web_page(SignUpStepDef.java:26)\r\n\tat ✽.Given I am in the automationpractice web page(signup.feature:5)\r\nCaused by: org.apache.http.conn.HttpHostConnectException: Connect to 127.0.0.1:7055 [/127.0.0.1] failed: Connection refused: connect\r\n\tat org.apache.http.impl.conn.DefaultHttpClientConnectionOperator.connect(DefaultHttpClientConnectionOperator.java:151)\r\n\tat org.apache.http.impl.conn.PoolingHttpClientConnectionManager.connect(PoolingHttpClientConnectionManager.java:353)\r\n\tat org.apache.http.impl.execchain.MainClientExec.establishRoute(MainClientExec.java:380)\r\n\tat org.apache.http.impl.execchain.MainClientExec.execute(MainClientExec.java:236)\r\n\tat org.apache.http.impl.execchain.ProtocolExec.execute(ProtocolExec.java:184)\r\n\tat org.apache.http.impl.execchain.RetryExec.execute(RetryExec.java:88)\r\n\tat org.apache.http.impl.execchain.RedirectExec.execute(RedirectExec.java:110)\r\n\tat org.apache.http.impl.client.InternalHttpClient.doExecute(InternalHttpClient.java:184)\r\n\tat org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:71)\r\n\tat org.apache.http.impl.client.CloseableHttpClient.execute(CloseableHttpClient.java:55)\r\n\tat org.openqa.selenium.remote.internal.ApacheHttpClient.fallBackExecute(ApacheHttpClient.java:161)\r\n\tat org.openqa.selenium.remote.internal.ApacheHttpClient.execute(ApacheHttpClient.java:89)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:134)\r\n\tat org.openqa.selenium.firefox.internal.NewProfileExtensionConnection.execute(NewProfileExtensionConnection.java:168)\r\n\tat org.openqa.selenium.firefox.FirefoxDriver$LazyCommandExecutor.execute(FirefoxDriver.java:392)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:578)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:306)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:483)\r\n\tat org.openqa.selenium.support.events.EventFiringWebDriver$2.invoke(EventFiringWebDriver.java:102)\r\n\tat com.sun.proxy.$Proxy15.get(Unknown Source)\r\n\tat org.openqa.selenium.support.events.EventFiringWebDriver.get(EventFiringWebDriver.java:160)\r\n\tat com.stepdefinations.SignUpStepDef.i_am_in_the_automationpractice_web_page(SignUpStepDef.java:26)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:483)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:37)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:13)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:31)\r\n\tat cucumber.runtime.java.JavaStepDefinition.execute(JavaStepDefinition.java:37)\r\n\tat cucumber.runtime.StepDefinitionMatch.runStep(StepDefinitionMatch.java:37)\r\n\tat cucumber.runtime.Runtime.runStep(Runtime.java:298)\r\n\tat cucumber.runtime.model.StepContainer.runStep(StepContainer.java:44)\r\n\tat cucumber.runtime.model.StepContainer.runSteps(StepContainer.java:39)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:48)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:91)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:93)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:37)\r\n\tat org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)\r\n\tat org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:363)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:98)\r\n\tat org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:50)\r\n\tat org.eclipse.jdt.internal.junit.runner.TestExecution.run(TestExecution.java:38)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:459)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:675)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:382)\r\n\tat org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:192)\r\nCaused by: java.net.ConnectException: Connection refused: connect\r\n\tat java.net.DualStackPlainSocketImpl.waitForConnect(Native Method)\r\n\tat java.net.DualStackPlainSocketImpl.socketConnect(DualStackPlainSocketImpl.java:85)\r\n\tat java.net.AbstractPlainSocketImpl.doConnect(AbstractPlainSocketImpl.java:345)\r\n\tat java.net.AbstractPlainSocketImpl.connectToAddress(AbstractPlainSocketImpl.java:206)\r\n\tat java.net.AbstractPlainSocketImpl.connect(AbstractPlainSocketImpl.java:188)\r\n\tat java.net.PlainSocketImpl.connect(PlainSocketImpl.java:172)\r\n\tat java.net.SocksSocketImpl.connect(SocksSocketImpl.java:392)\r\n\tat java.net.Socket.connect(Socket.java:589)\r\n\tat org.apache.http.conn.socket.PlainConnectionSocketFactory.connectSocket(PlainConnectionSocketFactory.java:74)\r\n\tat org.apache.http.impl.conn.DefaultHttpClientConnectionOperator.connect(DefaultHttpClientConnectionOperator.java:134)\r\n\t... 62 more\r\n",
-  "status": "failed"
+  "duration": 1861096543,
+  "status": "passed"
 });
 formatter.match({
   "location": "SignUpStepDef.i_move_to_register_page()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 18740433343,
+  "status": "passed"
 });
 formatter.match({
   "location": "SignUpStepDef.enter_valid_credentials_then_hit_submit()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 49605,
+  "status": "passed"
 });
 formatter.match({
   "location": "SignUpStepDef.user_should_register_successfully()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 38316,
+  "status": "passed"
 });
 });
